7d215e57dabae1cbc6bff6f7c6a6122694c4e206
Pearse Murphy <pearsemurphy04@gmail.com>
2025-12-03 12:22:07 +0000

    clean commented code

--- Dec02/solution.py
+++ Dec02/solution.py
@@ -4,15 +4,10 @@ def factorise(n):
     factors = []
     for i in range(1,n):
         if n%i == 0:
-            if i != n//i:
-                factors.append(i)
-                # factors.append(n//i)
-            else:
-                factors.append(i)
+            factors.append(i)
     return sorted(factors)
 
 def check_invalid(id_str:str) -> Bool:
-    # print(id_str)
     id_len = len(id_str)
     if id_len == 2 or id_len == 3:
             id_factors = [1]
@@ -21,13 +16,6 @@ def check_invalid(id_str:str) -> Bool:
     
     for factor in id_factors:
         id_split = [id_str[j:j+factor] for j in range(0,len(id_str),factor)]
-        # for id_part in id_split:
-        #     if id_part[0] == "0":
-        #         continue
-        # print(f"split {id_str} with {factor} factors: {id_split}")
-        # sum_split = sum(int(id_part) for id_part in id_split)
-        # inverse_factor = id_len//factor
-        # if sum_split/inverse_factor == int(id_split[0]):
         if all(id_part == id_split[0] for id_part in id_split):
             print(f"ID {id_str} invalid!")
             return True
@@ -57,12 +45,12 @@ for input_range in inputs:
         if part1:
             if id_len%2 == 1:
                     continue
-            # print(id_str[:id_len//2], id_str[id_len//2:])
+
             if id_str[:id_len//2] == id_str[id_len//2:]:
                 print(f"ID {id_str} invalid!")
                 invalid_ids.append(i)
         else:
-            # print(id_str)
+
             if check_invalid(id_str):
                     invalid_ids.append(i)
 
